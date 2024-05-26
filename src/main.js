a3a737abbd7bab4db6dba376b2c2bb4eb3aa7c17
SafaeHaj <safae.haj@gmail.com>
2024-05-26 13:23:50 +0100

    modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 
+library.add(fas);
 createApp(App)
     .use(router)
     .component('font-awesome-icon', FontAwesomeIcon)
